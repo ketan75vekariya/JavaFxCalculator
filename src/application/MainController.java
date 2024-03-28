9c5af9bc7f6e0b8fc8b692b423ccd05d53dfcfd4
ketan75vekariya <ketan75vekariya@gmail.com>
2024-03-28 12:20:05 +0000

    Basic Structure of calculator project

---
+++ src/application/MainController.java
@@ -0,0 +1,15 @@
+package application;
+
+import javafx.event.ActionEvent;
+
+public class MainController {
+	public void processOperator(ActionEvent event) {
+		
+	}
+	
+	public void processNumber(ActionEvent event) {
+		
+		
+	}
+
+}
---
+++ src/application/Modal.java
@@ -0,0 +1,11 @@
+package application;
+
+public class Modal {
+	public float calculator(long number1, long number2, String operator) {
+		
+		
+		return 0;
+		
+	}
+
+}
