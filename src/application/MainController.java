d2b9f0275e6ab71e57dfe4ef873ceef4ea89eae2
ketan75vekariya <ketan75vekariya@gmail.com>
2024-03-28 13:34:19 +0000

    Complete project

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -1,14 +1,43 @@
 package application;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class MainController {
-	public void processOperator(ActionEvent event) {
-		
-	}
 	
+	@FXML
+	private Label result;
+	private long number1 = 0;
+	private String operator ="";
+	private boolean start = true;
+	private Modal modal = new Modal();
+	
+	public void processOperator(ActionEvent event) {
+		String value =((Button)event.getSource()).getText();
+		if(!value.equals("=")) {
+			if(!operator.isEmpty()) return;
+			operator =value;
+			number1= Long.parseLong(result.getText());
+			result.setText("");
+		}else {
+			if(operator.isEmpty())
+				return;
+			long number2 = Long.parseLong(result.getText());
+			float output = modal.calculator(number1, number2, operator);
+			result.setText(String.valueOf(output));
+			operator = "";
+			start = true;
+		}
+	}
 	public void processNumber(ActionEvent event) {
-		
+		if(start) {
+			result.setText("");
+			start = false;
+		}
+		String value = ((Button)event.getSource()).getText();
+		result.setText(result.getText() + value);
 		
 	}
 
--- src/application/Modal.java
+++ src/application/Modal.java
@@ -3,8 +3,14 @@ package application;
 public class Modal {
 	public float calculator(long number1, long number2, String operator) {
 		
+		switch(operator) {
+		case "+":return  number1 + number2;
+		case "-":return  number1 - number2;
+		case "*":return  number1 * number2;
+		case "/":if(number2 == 0)return 0; return  number1 + number2;
+		default:return 0;
+		}
 		
-		return 0;
 		
 	}
 
